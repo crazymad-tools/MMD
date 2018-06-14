2e973cb11904f62112a2e43d82e270a7503d72b3
crazy_mad <crazymad0601@gmail.com>
2018-06-15 00:27:25 +0800

    version 2.0 start

---
+++ js/markdown.js
@@ -0,0 +1,16 @@
+/**
+ * @Class MdParse
+ * @constructor
+ * @description 返回md解析器
+ */
+function MdParse() {
+    this.flag = false;
+}
+
+MdParse.prototype.parseText = function() {
+    return;
+}
+
+MdParse.prototype.parseOneLine = function() {
+    alert(this.flag);
+}
